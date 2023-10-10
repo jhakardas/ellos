24dcef0d2a75abf8ef60537c76f70cb712b01771
jhakardas <127119460+jhakardas@users.noreply.github.com>
2023-10-10 21:14:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Location: https://main.d1v273nofwzkjm.amplifyapp.com/index.htm');
+header('Location: https://www.walmart.com/ip/Gocio-Adult-Electric-Bicycles-Foldable-Ebike-500W-26-Commuter-Bicycle-48V-Battery-Full-Suspension-Folding-Mountain-Bike-Adjustable-21-Speed-E-Bikes-A/137445417');
 
 exit;
 
